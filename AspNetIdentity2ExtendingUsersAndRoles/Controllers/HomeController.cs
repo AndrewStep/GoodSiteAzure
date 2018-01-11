3053850ad0b8b2d4a408dfe6da231a279faf16ec
student <student@K42-05>
2018-01-11 21:08:56 +0200

    sdgsdfh

--- AspNetIdentity2ExtendingUsersAndRoles/Controllers/HomeController.cs
+++ AspNetIdentity2ExtendingUsersAndRoles/Controllers/HomeController.cs
@@ -6,10 +6,10 @@ namespace IdentitySample.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
 
         [Authorize]
         public ActionResult About()
@@ -25,23 +25,23 @@ namespace IdentitySample.Controllers
 
             return View();
         }
-        //CarContext context;
-        //public HomeController()
-        //{
-        //    context = new CarContext();
-        //}
-        //// GET: Car
-        //public ActionResult Index(int id = 1)
-        //{
-        //    ViewBag.Count = context.CARSINFO.ToList().Count();
-        //    var model = context.CARSINFO;
-        //    ViewBag.Index = id;
-        //    return View();
-        //}
-        //public PartialViewResult GetCarsList(int id = 1)
-        //{
-        //    var res = context.CARSINFO.ToList().Skip((id - 1) * 2).Take(2);
-        //    return PartialView(res);
-        //}
+        CarContext context;
+        public HomeController()
+        {
+            context = new CarContext();
+        }
+        // GET: Car
+        public ActionResult Index(int id = 1)
+        {
+            ViewBag.Count = context.CARSINFO.ToList().Count();
+            var model = context.CARSINFO;
+            ViewBag.Index = id;
+            return View();
+        }
+        public PartialViewResult GetCarsList(int id = 1)
+        {
+            var res = context.CARSINFO.ToList().Skip((id - 1) * 2).Take(2);
+            return PartialView(res);
+        }
     }
 }
